fcab7309297d3ec12eea3a9e2f84af6781bd1453
hoangtuyb96 <hoangtuyb96@gmail.com>
2018-11-30 22:19:05 +0700

    fix_some_link_to

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by id: params[:id]
+    unless @user.present?
+      flash[:danger] = t ".cant_find"
+      redirect_to root_path
+    end
   end
 end
